082f7925acaea83463e57870a4cc56a09d147411
Rafas-ms <rafael.martinssilva@hotmail.com>
2021-11-25 15:39:09 -0300

    Criando a classe de teste da segunda parte

--- test/PrimeiraParte.java
+++ test/PrimeiraParte.java
@@ -6,15 +6,19 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author Seidor
- */
-public class PrimeiroPasso extends TestCase {
+
+Cenário Gherkin:
+
+Funcionalidade: Busca no Banco de Questões
+Cenário: Busca por questão inexistente
+Dado que navego para a página de busca do banco de questões
+E digito 'Science: Computers' no campo de busca
+Quando clico no botão de buscar
+Então visualizo uma mensagem de erro com o texto 'No questions found.'
+
+*/
+
+public class PrimeiraParte extends TestCase {
 
     public void teste() {
 
@@ -23,13 +27,14 @@ public class PrimeiroPasso extends TestCase {
         File file = new File("C:\\Users\\Seidor\\Desktop\\Projetos java\\chromedriver.exe"); //Coloque o caminho para o seu chromedrive
         System.setProperty("webdriver.chrome.driver", file.getAbsolutePath()); //Especifica o driver de navegação
         WebDriver driver = new ChromeDriver(); //Iniciando driver de navegação
+        
         driver.get("https://opentdb.com/browse.php");// Link para busca 
-
         driver.findElement(By.id("query")).sendKeys(question);//Selecionar a label de pesquisa pelo ID e preenche ela
         driver.findElement(By.xpath("//*[@id=\"page-top\"]/div[1]/form/div/button")).click();//Selecionar botão pelo caminho xpath
 
         String texto = driver.findElement(By.xpath("//*[@id=\"page-top\"]/div[2]/div")).getText(); // Pega o resultado da pesquisa e coloca em uma string
         assertEquals(texto, "No questions found."); // Compara o resultado obtido com o resultado esperado
+        
         driver.close();//Fechar o driver após a execução das atividades
  
     }
